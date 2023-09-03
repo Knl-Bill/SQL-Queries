d6882cb262db3168c4bed4f3bbcf845a112288ad
Berserk Geek <115896029+Knl-Bill@users.noreply.github.com>
2023-09-03 14:23:03 +0530

    Aggregate Functions like avg(), count(), etc.

--- Aggregate_Functions.sql
+++ Aggregate_Functions.sql
@@ -1,33 +1,40 @@
-#LAB3 
-#1
+# 1. Display the average age of sailors.
+  
 use db;
 select avg(sailor.age) from sailor;
 
-#2
+# 2. Find the average of distinct age of sailors.
+  
 select distinct avg(sailor.age) from sailor;
 
-#3
+# 3. Find the age of the oldest sailor.
+  
 use db;
 select sailor.age from sailor order by sailor.age DESC
 limit 1;
 
-#4
+# 4. Find the age of the youngest sailor.
+  
 use db;
 select sailor.age from sailor order by sailor.age 
 limit 1;
 
-#5
+# 5. Find the average age of sailor with a rating of 10.
+  
 use db;
 select avg(sailor.age) from sailor where sailor.rating in (10);
 
-#6
+# 6. Count the number of different sailor names.
+  
 use db;
 select distinct  count(sailor.sname) from sailor;
 
-#7
+# 7. Find the name and age of the oldest sailor.
+  
 use db;
 select sailor.sname, sailor.age from sailor order by sailor.age DESC limit 1;
 
-#8
+# 8. Count the number of sailors.
+  
 use db;
 select distinct count(sailor.sid) from sailor;
